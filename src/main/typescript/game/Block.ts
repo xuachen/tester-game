b4b5589e3fd9e1094f6ae238d596605fd1b328ee
Jullyana Mattos Vycas <jumattos@microsoft.com>
2019-02-05 12:00:09 -0800

    New class: Block

---
+++ src/main/typescript/game/Block.ts
@@ -0,0 +1,11 @@
+import GameObject from './GameObject'
+
+export default class Block extends GameObject {
+    constructor(x: number, y: number) {
+        super(x, y);
+    }
+
+    interact(): void {
+        console.log('You get a coin!');
+    }
+}
