1c2218b1a0a6268cf1d154fd9a66af7e912e2fbe
Jullyana Mattos Vycas <jumattos@microsoft.com>
2019-03-25 12:30:14 -0700

    Add build task for typescript + dialog to App

--- typescript/src/App.ts
+++ typescript/src/App.ts
@@ -1,10 +1,14 @@
 import Interactive from './Interactive'
 import GameObject from './GameObject'
+import Dialog from './Dialog'
 
 class App {
     main(): void {
         let block: Interactive = new GameObject(10, 10);
         block.interact();
+
+        let dialog: Interactive = new Dialog();
+        dialog.interact();
     }
 }
 
