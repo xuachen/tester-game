c3616cfdac7fe9074a6914d3c2c8819920d67963
Jullyana Mattos Vycas <jumattos@microsoft.com>
2019-09-10 11:05:54 -0700

    Clean up

--- typescript/src/App.ts
+++ typescript/src/App.ts
@@ -3,8 +3,6 @@ import GameObject from './GameObject'
 import Dialog from './Dialog'
 
 class App {
-    // TRIGGER ME PLS
-    // pretty please?
     main(): void {
         let block: Interactive = new GameObject(10, 10);
         block.interact();
