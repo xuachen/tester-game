fe398ab38403b0bc3c736e805eb8289b774c9eaf
Jullyana Mattos Vycas <jumattos@microsoft.com>
2019-09-11 09:21:13 -0700

    test

--- typescript/src/App.ts
+++ typescript/src/App.ts
@@ -3,6 +3,7 @@ import GameObject from './GameObject'
 import Dialog from './Dialog'
 
 class App {
+    // Yet another commit
     main(): void {
         let block: Interactive = new GameObject(10, 10);
         block.interact();
