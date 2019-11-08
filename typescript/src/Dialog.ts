e13a50c64b5408422dc5f365e25c02a678492257
Jullyana Mattos Vycas <jumattos@microsoft.com>
2019-11-08 09:36:38 -0800

    change

--- typescript/src/Dialog.ts
+++ typescript/src/Dialog.ts
@@ -2,7 +2,7 @@ import Interactive from './Interactive'
 
 export default class Dialog implements Interactive {
     interact(): void {
-        // Int release
+        // Int release, ops mishap trying again
         console.log("Interacting with dialog");
     }
 }
