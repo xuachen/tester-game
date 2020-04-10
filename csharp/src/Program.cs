d4c2101e56d0aac2fcfd04d9fdea795f8dcf858a
Jessica Petty <jepetty@microsoft.com>
2020-04-10 07:58:13 -0700

    Testing Rich Code Nav

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -6,6 +6,7 @@ namespace game
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Testing Rich Code Nav");
             Interactive block = new GameObject(10, 10);
             block.interact();
         }
