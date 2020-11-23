906241ab74bd7e87ad5061e3751fe2f8a7f94962
noecald <noelle.caldwell@microsoft.com>
2020-11-23 12:51:04 -0800

    test update for deployment 11/23/2020

--- java/src/main/java/com/test/sort/App.java
+++ java/src/main/java/com/test/sort/App.java
@@ -6,9 +6,9 @@ package com.test.sort;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
 
